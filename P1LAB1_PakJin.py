1d66fe54461e8e46410c54ab2b280daafce737b4
wls-fkaus <jihi061007@gmail.com>
2024-09-24 14:21:18 -0400

    First upload

---
+++ P1LAB1_PakJin.py
@@ -0,0 +1,15 @@
+# Jin Pak
+# 9/24/2024
+# P1LAB1
+# It shows how to get input from the user and display output.
+
+
+# Get first name from the user
+
+firstName = input("Enter your first name: ")
+print ()
+# Get last name
+lastName = input("Enter your last name: ")
+print ()
+# Display info
+print("Hello,", firstName, lastName + "! Welcome to CTI-110")
